4892a9ccfe86871c0b575e3bf05e1e78a3073152
Stipe Kotarac <stipe@kotarac.net>
2017-10-11 11:28:00 +0200

    movie syncer

--- sync.js
+++ sync.js
@@ -3,4 +3,15 @@ const _ = require('lodash')
 const db = require('db')
 const tmdb = require('tmdb')
 
-tmdb.getPaginated('/movie/popular').then(console.log)
+function getMovies () {
+  return tmdb.getPaginated('/movie/popular')
+}
+
+async function sync () {
+  const movies = await getMovies()
+  await Promise.map(movies, function (movie) {
+    return db.movie.put(movie.id, movie)
+  })
+  process.exit(0)
+}
+sync()
